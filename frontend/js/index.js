b3cfecb3b75ac9f39ebe510c932eee613568d59e
morteza <hadafmahdi@gmail.com>
2023-06-12 00:02:31 +0330

    add fixbug on header

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,14 +1,22 @@
 
-const mobileMenuElem = document.querySelector('.mobile-menu')
 
+// generate  mobile menu
+const mobileMenuElem = document.querySelector('.mobile-menu')
 const mainHeaderMobileMenuElem = document.querySelector('.main-header__mobile-menu')
 const closeMobileMenuElem = document.querySelector('.main-header__close-mobile-menu')
+
 
 
 mobileMenuElem.addEventListener('click',()=>{
     mainHeaderMobileMenuElem.classList.add("main-header__mobile-menu--active")
 })
+
 closeMobileMenuElem.addEventListener('click',()=>{
     mainHeaderMobileMenuElem.classList.remove("main-header__mobile-menu--active")
 })
 
+mainHeaderMobileMenuElem.addEventListener('click', event =>{
+    if(event.target.nextSibling.className){  
+        event.target.nextSibling.classList.toggle("main-header__mobile-menu-dropdown--active")
+    }
+})
