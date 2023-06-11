2df7e5ab1f585be314ec47a8c3db30fec223bdc0
morteza <hadafmahdi@gmail.com>
2023-06-11 21:25:34 +0330

    add topbar and navbar to home page

---
+++ frontend/js/index.js
@@ -0,0 +1,14 @@
+
+const mobileMenuElem = document.querySelector('.mobile-menu')
+
+const mainHeaderMobileMenuElem = document.querySelector('.main-header__mobile-menu')
+const closeMobileMenuElem = document.querySelector('.main-header__close-mobile-menu')
+
+
+mobileMenuElem.addEventListener('click',()=>{
+    mainHeaderMobileMenuElem.classList.add("main-header__mobile-menu--active")
+})
+closeMobileMenuElem.addEventListener('click',()=>{
+    mainHeaderMobileMenuElem.classList.remove("main-header__mobile-menu--active")
+})
+
